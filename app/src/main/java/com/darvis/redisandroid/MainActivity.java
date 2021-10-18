b9469ba16d4e76df98c8659522e9741f43892c4c
Sabeeh Zaidi <sabeehz@gmail.com>
2021-10-18 12:51:40 +0500

    Update MainActivity.java

--- app/src/main/java/com/darvis/redisandroid/MainActivity.java
+++ app/src/main/java/com/darvis/redisandroid/MainActivity.java
@@ -19,32 +19,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.d("SabeehDev", "Hello" +
-                "World ");
-//        new Handler().post(new Runnable() {
-//            @Override
-//            public void run() {
-//
-//                Redis r = null;
-//                try {
-//                    r = new Redis(new Socket("127.0.0.1", 6379));
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//                try {
-//                    r.call("SET", "foo", "123");
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//
-//                try {
-//                    System.out.println(r.call("GET", "foo")); // will print '579'
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        });
-
         new AsyncTaskRunner().execute();
     }
 
@@ -56,8 +30,7 @@ public class MainActivity extends AppCompatActivity {
             Redis r = null;
             byte[] result = null;
             try {
-//                r = new Redis(new Socket("127.0.0.1", 6379));
-                r = new Redis(new Socket("192.168.100.20", 6379));
+                r = new Redis(new Socket("192.168.100.20", 6379)); //IP Address and Port
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -69,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
             }
 
             try {
-                result = r.call("GET", "foo"); // will print '579'
+                result = r.call("GET", "foo"); // will print '123'
             } catch (IOException e) {
                 e.printStackTrace();
             }
